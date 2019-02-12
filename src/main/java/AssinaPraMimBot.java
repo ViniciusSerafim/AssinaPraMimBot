3fbfe9aecbb6133098a6dc84a4149eb1e89bc433
yendorr <ro.rss.129@gmail.com>
2019-02-12 19:19:31 -0200

    tem botoes porem ainda inuteis

--- src/main/java/AssinaPraMimBot.java
+++ src/main/java/AssinaPraMimBot.java
@@ -1,14 +1,92 @@
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-import org.telegram.telegrambots.api.methods.send.*;
 import org.telegram.telegrambots.api.objects.*;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AssinaPraMimBot extends TelegramLongPollingBot {
-    int num;
+
+
     public void onUpdateReceived(Update update) {
+//        manda1(update);
+        manda2(update);
+//        numerico(update);
+        botao(update);
+    }
 
+    public Integer botao(Update update){
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            if (true) {
+                SendMessage message = new SendMessage() // Create a message object object
+                        .setChatId(chat_id)
+                        .setText("You send /start");
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                List<List<InlineKeyboardButton>> rowsInline = new ArrayList<List<InlineKeyboardButton>>();
+                List<InlineKeyboardButton> rowInline = new ArrayList<InlineKeyboardButton>();
+                rowInline.add(new InlineKeyboardButton().setText("Materia 1").setCallbackData("ae"));
+                rowInline.add(new InlineKeyboardButton().setText("Materia 2").setCallbackData("mahoeh"));
+                rowInline.add(new InlineKeyboardButton().setText("Materia 3").setCallbackData("aeaeae"));
+                rowInline.add(new InlineKeyboardButton().setText("Materia 4").setCallbackData("uepa"));
+                rowInline.add(new InlineKeyboardButton().setText("Materia 5").setCallbackData("HA"));
+                rowInline.add(new InlineKeyboardButton().setText("Materia 6").setCallbackData("ALAMANO"));
+                // Set the keyboard to the markup
+                rowsInline.add(rowInline);
+                // Add it to the message
+                markupInline.setKeyboard(rowsInline);
+                message.setReplyMarkup(markupInline);
+                try {
+                    execute(message); // Sending our message object to user
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else {
 
+            }
+
+        } else if (update.hasCallbackQuery()) {}
+        return 0;
+    }
+
+    public Integer numerico(Update update) {
+
+        ReplyKeyboardMarkup keyBoardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+        KeyboardRow row = new KeyboardRow();
+        for (int i = 1; i < 10; i++) {
+            row.add(Integer.valueOf(i).toString());
+            if (i % 3 == 0) {
+                keyboard.add(row);
+                row = new KeyboardRow();
+            }
+        }
+        row.add("0");
+        keyboard.add(row);
+        keyBoardMarkup.setKeyboard(keyboard);
+        keyBoardMarkup.setResizeKeyboard(true);
+        Message message = update.getMessage();
+        SendMessage msgrequest = new SendMessage();
+        msgrequest.setChatId(message.getChatId());
+//        msgrequest.setText(message.getText());
+        msgrequest.setReplyMarkup(keyBoardMarkup);
+        try {
+            sendMessage(msgrequest); //at the end, so some magic and send the message ;)
+        } catch (TelegramApiException e) {
+            //do some error handling
+        }
+        return 0;
+    }
+
+    public Integer manda1(Update update){
         if(update.hasMessage()){
             Message message = update.getMessage();
 
@@ -23,16 +101,19 @@ public class AssinaPraMimBot extends TelegramLongPollingBot {
                     sendMessage(sendMessageRequest); //at the end, so some magic and send the message ;)
                 } catch (TelegramApiException e) {
                     //do some error handling
-                }//end catch()
-            }//end if()
-        }//end  if()
+                }
+            }
+        }
+        return 0;
+    }
 
+    public Integer manda2(Update update){
         //check if the update has a message
         if (update.hasMessage() && update.getMessage().hasText()) {
 
             //String message_text = update.getMessage().getText();
             //String message_text = update.getMessage().getChatId().toString();
-            String message_text = "Vai tomar no seu cu";
+            String message_text = "UEPA";
             long chat_id = update.getMessage().getChatId();
 
             SendMessage message = new SendMessage() // Create a message object object
@@ -44,8 +125,8 @@ public class AssinaPraMimBot extends TelegramLongPollingBot {
                 e.printStackTrace();
             }
         }
+        return 0;
     }
-
 
     public String getBotUsername() {
         return "Tonias";
@@ -54,6 +135,4 @@ public class AssinaPraMimBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return "771756087:AAGV5irFeIYnq0-amtP2f0SkITc4NRoqRXo";
     }
-
-
 }
